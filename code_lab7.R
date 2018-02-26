358635d63878bdcb7e3e58d8cf5a8ddb75484465
ngtrang <m.trang91@gmail.com>
2018-02-26 16:04:49 +0300

    first commit
    add Annotation people and places

--- code_lab7.R
+++ code_lab7.R
@@ -1,14 +1,16 @@
 library(NLP)
 library(openNLP)
 library(tm)
-#library(Rweka)
+library(Rweka)
 library(magrittr)
+
 #open data file to read
 readfile <- file("F:/PhD/R_guide/data/Leo_Tolstoy.txt",open = "r")
 bio <- readLines(readfile)
 #close data file
 close(readfile)
-#dding a space between each of lines 
+
+#Adding a space between each of lines 
 bio <- paste(bio, collapse = " ")
 #convert bio variable to a string
 bio <- as.String(bio)
@@ -29,5 +31,22 @@ head(bio_ann)
 bio_doc <- AnnotatedPlainTextDocument(bio, bio_ann)
 
 #extract information from document
-sents(bio_doc) %>% head(2)
-words(bio_doc) %>% head(10)
+sents(bio_doc) %>% head(2) #extract 2 first sentences.
+words(bio_doc) %>% head(10) #extract 10 first words.
+
+#create annatation people and location.
+#Note! - we need install package openNLPmodels.en for English
+# install.packages("http://datacube.wu.ac.at/src/contrib/openNLPmodels.en_1.5-1.tar.gz",repos=NULL, type="source")
+#
+person_ann <- Maxent_Entity_Annotator(kind = "person")
+location_ann <- Maxent_Entity_Annotator(kind = "location")
+pipeline <- list(sent_ann,
+                 word_ann,
+                 person_ann,
+                 location_ann)
+bio_ann <- annotate(bio, pipeline)
+bio_doc <- AnnotatedPlainTextDocument(bio, bio_ann)
+
+#extract entities from AnnotatedPlainTextDocument
+entities(bio_doc, kind = "person") #extract names
+entities(bio_doc, kind = "location") #extract location
